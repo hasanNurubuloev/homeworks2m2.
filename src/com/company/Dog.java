45cf3f2eed9e3d89d119ea51f7a26d4c8aad1e53
hasanNurubuloev <hasan.nurubuloev59@gmail.com>
2019-09-25 21:50:18 +0600

    kk

---
+++ src/com/company/Dog.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Dog extends Animal implements Printable {
+
+
+    private String makeVoice ;
+
+    public Dog(String makeVoice) {
+        setName("Rex");
+
+        this.makeVoice = makeVoice;
+    }
+
+    @Override
+    public void print() {
+        System.out.println(getName() + "'s voice is " + getMakeVoice() );
+    }
+
+    public String getMakeVoice() {
+        return makeVoice;
+    }
+
+
+}
+
